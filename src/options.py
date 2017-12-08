1c9ae65780f168897ba8e9a8b6a50e62c396fdd8
Alex Siryi <desertkun@gmail.com>
2017-12-08 17:55:29 +0200

    Game Master and Game Controller services are now split
    See https://github.com/anthill-platform/anthill-game-master

--- src/options.py
+++ src/options.py
@@ -31,7 +31,7 @@ define("sock_path",
        type=str)
 
 define("binaries_path",
-       default="/opt/local/anthill/game-binaries",
+       default="/usr/local/anthill/game-controller-binaries",
        help="Location of game server binaries.",
        type=str)
 
